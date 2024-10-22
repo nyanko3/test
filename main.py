ba61879bcbdabc41cc1e536607e4043571b96d96
nyanko3 <nemusugiru2@outlook.com>
2024-10-22 12:10:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import ast
 
 max_api_wait_time = 3
 max_time = 10
-apis = [r"https://invidious.nietzospannend.nl/",r"https://invidious.pcgamingfreaks.at/",r"https://invidious.perennialte.ch/"]
+apis = [r"https://invidious.pcgamingfreaks.at/",r"https://invidious.perennialte.ch/"]
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
