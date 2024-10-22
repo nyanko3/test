48805399bb8597014b1e5a66d3d18e616dee61cd
nyanko3 <nemusugiru2@outlook.com>
2024-10-22 11:34:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import ast
 
 max_api_wait_time = 3
 max_time = 10
-apis = [r"https://invidious.drgns.space/",r"https://invidious.einfachzocken.eu/",r"https://invidious.ethibox.fr/"]
+apis = [r"https://invidious.einfachzocken.eu/",r"https://invidious.ethibox.fr/"]
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
