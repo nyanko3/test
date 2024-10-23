18c4ddcf8cfab86c870d19d45baa6750778ac6e5
nyanko3 <nemusugiru2@outlook.com>
2024-10-23 11:11:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import ast
 
 max_api_wait_time = 3
 max_time = 10
-apis = ['https://invidious.flokinet.to/']
+apis = ['https://y.com.sb/']
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
