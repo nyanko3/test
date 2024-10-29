64c1849e5500db47e6353fce213a742297505cf8
眠いなぁ <nemusugiru2@outlook.com>
2024-10-29 10:48:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from cache import cache
 
 
 max_api_wait_time = 6
-max_time = 10
-apis = [r'https://invidious.jing.rocks/']
+　max_time = 10
+apis = [r'https://youtube.privacyplz.org/']
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
