ffbd37cb240fe44f7d9f22af8c902ea65fbf74df
眠いなぁ <nemusugiru2@outlook.com>
2024-10-29 11:35:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from cache import cache
 
 max_api_wait_time = 6
 max_time = 10
-apis = [r'https://invidious.nietzospannend.nl/']
+apis = [r'https://invidious.ethibox.fr/']
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
