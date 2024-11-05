6439f3fbd1911608c59b4c7f256634f9e7ee7d71
眠いなぁ <nemusugiru2@outlook.com>
2024-11-05 12:54:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,15 @@ from cache import cache
 
 max_api_wait_time = 3
 max_time = 10
-apis = [r"https://invidious.drgns.space/",
-r"https://invidious.einfachzocken.eu/",]
+apis = [r"https://inv.vern.cc/",
+r"https://invi.susurrando.com/",
+r"https://invidio.xamh.de/",
+r"https://invidious.0011.lt/",
+r"https://invidious.adminforge.de/",
+r"https://invidious.baczek.me/",
+r"https://invidious.chunboan.zone/",
+r"https://invidious.drgns.space/",
+r"https://invidious.einfachzocken.eu/"]
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
